c52720c5c04b4bf09ac71f39c587e5749b3deaa9
JOSH <onyemajoshua892@gmail.com>
2022-07-22 13:26:01 +0100

    save

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ buttonn.addEventListener("click", myFunction);
 
 function myFunction() {
     const val = document.getElementById("tracker").value;
-    if (val === "75Ag85tGw") {
+    if (val == "75Ag85tGw") {
         window.location.href = "http://dartfreightlogistics.com/track3.html";
     } else {
         window.location.href = "http://dartfreightlogistics.com/track.html";
