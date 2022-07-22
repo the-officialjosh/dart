b655021542a07f0653657a5bd6fda3657566b11c
JOSH <onyemajoshua892@gmail.com>
2022-07-22 20:13:55 +0100

    save

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ buttonn.addEventListener("click", myFunction);
 function myFunction() {
     const val = document.getElementById("tracker").value;
     const vals = document.getElementById("trackered").value;
-    if (val === "75Ag85tGw") {
-        window.location.href = "http://dartfreightlogistics.com/track3.html";
-    } else if (vals === "75Ag85tGw") {
+    if (val === "75Ag85tGw" || vals === "75Ag85tGw") {
         window.location.href = "http://dartfreightlogistics.com/track3.html";
     } else {
         window.location.href = "http://dartfreightlogistics.com/track.html";
