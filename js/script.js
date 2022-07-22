82f9c45683f49ce281e1bbbb3f8401a320450d3f
JOSH <onyemajoshua892@gmail.com>
2022-07-22 21:53:46 +0100

    save

--- js/script.js
+++ js/script.js
@@ -19,3 +19,8 @@ valed.addEventListener("keydown", function(e) {
         window.location.href = "http://dartfreightlogistics.com/track.html";
     }
 });
+// const form = document.querySelector("form");
+// form.addEventListener("submit", (event) => {
+//     // stop form submission
+//     event.preventDefault();
+// });
